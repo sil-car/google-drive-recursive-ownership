fb386b70b0d0a97a4e60384ded84557c2b0dfdbb
Nate Marti <nate_marti@sil.org>
2021-04-27 11:40:26 +0100

    reduce output; avoid trawling entire My Drive

--- new-owner.py
+++ new-owner.py
@@ -18,8 +18,8 @@ def get_drive_service():
     flow = oauth2client.client.flow_from_clientsecrets(CLIENT_SECRETS, OAUTH2_SCOPE)
     flow.redirect_uri = oauth2client.client.OOB_CALLBACK_URN
     authorize_url = flow.step1_get_authorize_url()
-    print(f'Use this link for authorization: {authorize_url}')
-    code = six.moves.input('Verification code: ').strip()
+    print(f'\nUse this link for authorization: {authorize_url}')
+    code = six.moves.input('\nAuth. code: ').strip()
     credentials = flow.step2_exchange(code)
     http = httplib2.Http()
     credentials.authorize(http)
@@ -36,7 +36,7 @@ def get_permission_id_for_email(service, email):
 def show_info(service, drive_item, prefix, permission_id):
     try:
         print(os.path.join(prefix, drive_item['title']))
-        print('Would set new owner to {}.'.format(permission_id))
+        print(f'Would set new owner to {permission_id}.')
     except KeyError:
         print('No title for this item:')
         pprint.pprint(drive_item)
@@ -49,26 +49,33 @@ def grant_ownership(service, drive_item, prefix, permission_id, show_already_own
     for owner in drive_item['owners']:
         if owner['permissionId'] == permission_id:
             if show_already_owned:
-                print(f'Item "{full_path}" already has the right owner.')
+                print(f'Ownership already correct for "{full_path}".')
             return
         elif owner['isAuthenticatedUser']:
             current_user_owns = True
 
-    print(f'Item "{full_path}" needs ownership granted.')
+    print(f'Changing ownership for "{full_path}"... ', end='', flush=True)
 
     if not current_user_owns:
-        print('    But, current user does not own the item.')
+        print('Skipped: not owned by current user.')
         return
 
     try:
         permission = service.permissions().get(fileId=drive_item['id'], permissionId=permission_id).execute()
         permission['role'] = 'owner'
-        print('    Upgrading existing permissions to ownership.')
-        return service.permissions().update(fileId=drive_item['id'], permissionId=permission_id, body=permission, transferOwnership=True).execute()
+        result = service.permissions().update(fileId=drive_item['id'], permissionId=permission_id, body=permission, transferOwnership=True).execute()
+        print('Done.')
+        if len(prefix) < 1:
+            # Top level folder is changed last. Exit script to avoid additional
+            #   "My Drive" trawling.
+            exit()
+        return result
     except googleapiclient.errors.HttpError as e:
         if e.resp.status != 404:
-            print(f'An error occurred updating ownership permissions: {e}')
+            print(f'Error: {e}')
             return
+        else:
+            print("Server error 404.")
 
     print('    Creating new ownership permissions.')
     permission = {'role': 'owner',
@@ -99,12 +106,16 @@ def process_all_files(service, callback=None, callback_args=None, minimum_prefix
             for child in children.get('items', []):
                 item = service.files().get(fileId=child['id']).execute()
                 #pprint.pprint(item)
+                if folder_id == 'root' and item['title'] != minimum_prefix[0]:
+                    # Skip irrelevant files and folders in "My Drive".
+                    # print(f"Skipping \"{item['title']}\"")
+                    continue
                 if item['kind'] == 'drive#file':
                     if current_prefix[:len(minimum_prefix)] == minimum_prefix:
-                        print(f"File: \"{item['title']}\" ({current_prefix}, {item['id']})")
+                        # print(f"File: \"{item['title']}\" ({current_prefix}, {item['id']})")
                         callback(service, item, current_prefix, **callback_args)
                     if item['mimeType'] == 'application/vnd.google-apps.folder':
-                        print(f"Folder: \"{item['title']}\" ({current_prefix}, {item['id']})")
+                        # print(f"Folder: \"{item['title']}\" ({current_prefix}, {item['id']})")
                         next_prefix = current_prefix + [item['title']]
                         comparison_length = min(len(next_prefix), len(minimum_prefix))
                         if minimum_prefix[:comparison_length] == next_prefix[:comparison_length]:
@@ -120,16 +131,16 @@ def process_all_files(service, callback=None, callback_args=None, minimum_prefix
 
 
 def main():
-    minimum_prefix = six.text_type(sys.argv[1])
+    minimum_prefix_text = six.text_type(sys.argv[1])
     new_owner = six.text_type(sys.argv[2])
     show_already_owned = False if len(sys.argv) > 3 and six.text_type(sys.argv[3]) == 'false' else True
-    print(f'Changing all files at path "{minimum_prefix}" to owner "{new_owner}"')
-    minimum_prefix_split = minimum_prefix.split(os.path.sep)
-    print(f'Prefix: {minimum_prefix_split}')
+    print(f'Changing all files at path "{minimum_prefix_text}" to owner "{new_owner}"')
+    minimum_prefix = minimum_prefix_text.split(os.path.sep)
+    print(f'Prefix: {minimum_prefix}')
     service = get_drive_service()
     permission_id = get_permission_id_for_email(service, new_owner)
     print(f'User {new_owner} is permission ID {permission_id}.')
-    process_all_files(service, grant_ownership, {'permission_id': permission_id, 'show_already_owned': show_already_owned }, minimum_prefix_split)
+    process_all_files(service, grant_ownership, {'permission_id': permission_id, 'show_already_owned': show_already_owned }, minimum_prefix)
     #print(files)
 
 
